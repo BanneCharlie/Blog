325802f20bce3ba5f190c2862f0ad256a63c8644
1876485877@qq.com <bannecharlie@gmail.com>
2024-03-23 23:06:16 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,96 @@
+import { defineConfig } from "vitepress";
+import { set_sidebar } from "../utils/auto-gen-sidebar.mjs"; // 改成自己的路径
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  title: "CORSAIR | BLOG",
+  description: "雨露润余春，椛开落满城。",
+  base:"/Blog/",
+  markdown: {
+    // 代码块风格
+    // theme: 'material-theme-palenight',
+    theme:'github-light',
+    // 代码块显示行数
+    lineNumbers: true,
+  },
+  themeConfig: {
+    // 站点首页标题
+    siteTitle: "CORSAIR",
+    // 站点logo
+    logo: "/logo.svg",
+
+    // 导航栏
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Vue", link: "/docs/FrontEnd/Vue3/1.Grammar" },
+      { text: "Spring", link: "/docs/BackEnd/Spring/1.Info" },
+      { text: "SpringMVC", link: "/docs/BackEnd/SpringMVC/1.Info" },
+      { text: "Mybatis", link: "/docs/BackEnd/Mybatis/Mybatis" },
+
+      { text: "Mysql", link: "/docs/BackEnd/Mysql/1.Info" },
+      { text: "Redis", link: "/docs/BackEnd/Redis/1.Info" },
+
+      {text: "RabbitMQ",link: "/docs/SpringCloudAlibaba/5.RabbitMQ/RabbitMQ"},
+
+      { text: "Interview", link: "/docs/Interview/Interview" },
+    ],
+
+    // 右侧导航栏默认索引
+    outline: [1, 6],
+
+    // 通过插件自动生成,左边栏插件
+    sidebar: {
+      "/docs/FrontEnd/Vue3": set_sidebar("docs/FrontEnd/Vue3"),
+      "/docs/BackEnd/Spring": set_sidebar("docs/BackEnd/Spring"),
+      "/docs/BackEnd/Mybatis/Mybatis": set_sidebar("docs/BackEnd/Mybatis"),
+      
+      "/docs/BackEnd/Mysql": set_sidebar("docs/BackEnd/Mysql"),
+      "/docs/BackEnd/Redis": set_sidebar("docs/BackEnd/Redis"),
+      "/docs/Interview": set_sidebar("docs/Interview"),
+      "/docs/SpringCloudAlibaba/5.RabbitMQ/RabbitMQ": set_sidebar("docs/SpringCloudAlibaba/5.RabbitMQ"),
+
+      // 前端框架
+      "/docs/FrontEnd": set_sidebar("docs/FrontEnd"),
+
+      // 后端框架
+      "/docs/BackEnd": set_sidebar("docs/BackEnd"),
+
+      // 微服务的侧边栏
+      "/docs/SpringCloudAlibaba": set_sidebar("docs/SpringCloudAlibaba"),
+  
+    },
+    
+    // sidebar:false,
+    // aside: "left", // 设置右侧侧边栏在左侧显示
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/BanneCharlie/Note.git" },
+    ],
+
+    footer: {
+      copyright: "Copyright @ BANNE !",
+    },
+
+    // 设置搜索框
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    }, 
+  },
+
+});
---
+++ index.md
@@ -0,0 +1,40 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Banne's Blog"
+  text: "Set Sail"
+  tagline:  I'm Captain Jack Sparrow. The original. The only!
+  image:
+    src: /logo.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: FrontEnd
+      link: /docs/FrontEnd/Html_Css
+    - theme: alt
+      text: BackEnd
+      link: /docs/BackEnd/Java/1.JavaBasic
+    - theme: alt
+      text: SpringCloudAlibaba
+      link: /docs/SpringCloudAlibaba/0.Docker/Docker
+
+features:
+  # - title: <img src = "/mysql.svg" "></img>
+  #   details:  Game
+  #   link: /docs/Dino/dino
+
+  - title: <img src = "/vue.svg" "></img>
+    details: Vue.js, commonly referred to as Vue, is a popular front-end JavaScript framework used for building user interfaces and single-page applications
+    link: /docs/FrontEnd/Vue3/1.Grammar.md
+
+  - title: <img src = "/spring.svg" "></img>
+    details: Spring is a widely used open-source framework for building enterprise-level Java applications.
+    link: /docs/BackEnd/Spring/1.Info
+
+  - title: <img src = "/mysql.svg" "></img>
+    details: MySQL is an open-source relational database management system (RDBMS) that is widely used for managing and manipulating structured data.
+    link: /docs/BackEnd/Mysql/1.Info
+---
+
